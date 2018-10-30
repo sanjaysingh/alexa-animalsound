6196c4c2712b661de051ab43a87d33c89962fee5
itssanjay@gmail.com <itssanjay@gmail.com>
2018-10-29 22:52:24 -0400

    Authentication base changes

---
+++ RandomAnimalSounds/AlexaRequestAuthenticator.cs
@@ -0,0 +1,60 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs.Host;
+//using System;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace RandomAnimalSounds
+//{
+//    public static class AlexaRequestAuthenticator
+//    {
+//        private static HttpClient client = new HttpClient();
+
+//        public static async Task<AuthenticationResponse> Authenitcate(HttpRequest req, TraceWriter log)
+//        {
+//            var certChainUrl = req.SignatureCertChainUrl();
+
+//            if (!VerifyCertificateUrl(certChainUrl))
+//            {
+//                return AuthenticationResponse.InvalidSignatureCertChainUrl;
+//            }
+
+//            var pamEncodedCertText = await client.GetStringAsync(certChainUrl);
+//            var cert = new X509Certificate2(Encoding.UTF8.GetBytes(pamEncodedCertText));
+//            if(cert.NotBefore > DateTime.Today || cert.NotAfter <= DateTime.Today)
+//            {
+//                return AuthenticationResponse.ExpiredSignatureCert;
+//            }
+//            if(cert.GetSubjectAlternativeNames())
+
+//        }
+
+//        private static string SignatureCertChainUrl(this HttpRequest req) => req.Headers["SignatureCertChainUrl"].First();
+//        private static string Signature(this HttpRequest req) => req.Headers["Signature"].First();
+
+//        private static bool VerifyCertificateUrl(string certChainUrl)
+//        {
+//            if (string.IsNullOrWhiteSpace(certChainUrl))
+//            {
+//                return false;
+//            }
+
+//            Uri certChainUri;
+//            if (!Uri.TryCreate(certChainUrl, UriKind.Absolute, out certChainUri))
+//            {
+//                return false;
+//            }
+
+//            return
+//                certChainUri.Host.Equals("s3.amazonaws.com", StringComparison.OrdinalIgnoreCase) &&
+//                certChainUri.PathAndQuery.StartsWith("/echo.api/") &&
+//                certChainUri.Scheme == Uri.UriSchemeHttps &&
+//                certChainUri.Port == 443;
+//        }
+
+//    }
+//}
---
+++ RandomAnimalSounds/AuthenticationResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace RandomAnimalSounds
+{
+    public class AuthenticationResponse
+    {
+        private bool success = false;
+
+        public static AuthenticationResponse InvalidSignatureCertChainUrl => new AuthenticationResponse();
+
+        public static AuthenticationResponse ExpiredSignatureCert => new AuthenticationResponse();
+
+        public static AuthenticationResponse Success(string body) => new AuthenticationResponse() { success = true, Body = body };
+
+        public bool IsSuccess() => success;
+
+        public bool IsFailure() => !success;
+
+        public string Body { get; set; }
+    }
+}
